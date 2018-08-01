c94a7f281d412afff8e1e672b32a01c217836e9f
Sohel Shaikh <rebht78@gmail.com>
2018-08-01 21:10:26 +0530

    changes to index.js, card.js - added Card

--- src/Card.js
+++ src/Card.js
@@ -4,10 +4,13 @@ class Card extends Component {
     render() {
         return (
             <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-                <img src='' alt='Image of Superstar'/>
+                <h2>Rank: {this.props.rank}</h2>
                 <div>
-                    <h2>The Undertaker</h2>
-                    <p>undertaker@wwe.com</p>
+                    <img src={this.props.pic} alt='Superstar'/>
+                    <div>
+                        <h2>{this.props.name}</h2>
+                        <p>{this.props.email}</p>
+                    </div>
                 </div>
             </div>
         );
---
+++ src/superstars.js
@@ -0,0 +1,32 @@
+export const superstars = [
+    {
+        name:"The Rock",
+        email:"rock@wwe.com",
+        rank:0
+    },
+    {
+        name:"The Undertaker",
+        email:"theundertaker@wwe.com",
+        rank:1
+    },
+    {
+        name:"Roman Reigns",
+        email:"roman@wwe.com",
+        rank:2
+    },
+    {
+        name:"Seth Rollins",
+        email:"seth@wwe.com",
+        rank:3
+    },
+    {
+        name:"Dean Ambrose",
+        email:"deank@wwe.com",
+        rank:4
+    },
+    {
+        name:"The Miz",
+        email:"miz@wwe.com",
+        rank:100
+    }
+];
