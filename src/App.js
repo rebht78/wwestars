d423198a8e6d9235c9ee46f9255d45e6e684e59a
Sohel Shaikh <rebht78@gmail.com>
2018-08-04 22:23:10 +0530

    Added State to the root App component

--- src/App.js
+++ src/App.js
@@ -5,14 +5,27 @@ import {superstars} from './superstars';
 
 class App extends Component
 {
+    constructor() {
+        super();
+        this.state = {
+            superstars:superstars,
+            searchField:''
+        }
+    }
     render() {
+        const filteredSuperStar = this.state.superstars.filter((superstar) => {
+            return superstar.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        });
         return (
             <div className='tc'>
                 <h1>WWE Robot SuperStars</h1>
-                <SearchBox/>
-                <CardList superstars={superstars}/>
+                <SearchBox filter={this.onInputChange}/>
+                <CardList superstars={filteredSuperStar}/>
             </div>  
         );
+    }
+    onInputChange = (event) => {
+        this.setState({searchField: event.target.value});
     }
 }
 
