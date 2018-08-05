7c880faaea347bf4e4ed2e96145fe616b5360450
Sohel Shaikh <rebht78@gmail.com>
2018-08-05 12:55:54 +0530

    Added background styling for index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'tachyons';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
