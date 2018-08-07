2343482f00641275287a151c63ce9a37798529aa
Sohel Shaikh <rebht78@gmail.com>
2018-08-07 22:11:25 +0530

    working index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'C:/Users/aziz/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react';
-import ReactDOM from 'C:/Users/aziz/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import 'tachyons';
 import App from '../containers/App';
 import registerServiceWorker from './registerServiceWorker';
