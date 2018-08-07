978c8259fba3545e513284b43eb362a7ce8f3e4f
Sohel Shaikh <rebht78@gmail.com>
2018-08-07 22:08:09 +0530

    change index.js to import relevant component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'C:/Users/aziz/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react';
+import ReactDOM from 'C:/Users/aziz/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react-dom';
 import 'tachyons';
-import App from '.containers/App';
+import App from '..containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
