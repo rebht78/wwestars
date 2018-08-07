886faf67b3686226c4406dfed7f08a69f8198be8
Sohel Shaikh <rebht78@gmail.com>
2018-08-07 22:09:07 +0530

    imported relevant components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import CardList from '..components/CardList';
-import SearchBox from '..components/SearchBox';
-import ScrollBar from '..components/ScrollBar';
-import {superstars} from '..model/superstars';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import ScrollBar from '../components/ScrollBar';
+import {superstars} from '../model/superstars';
 import './App.css';
 
 class App extends Component
