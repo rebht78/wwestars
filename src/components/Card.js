20e174675a313af487c0cca5d2e8d3d3e786f8be
Sohel Shaikh <rebht78@gmail.com>
2018-08-09 22:51:08 +0530

    Added Vote Now button for Card Component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import ActionButton from './ActionButton';
 
 class Card extends Component {
+
     render() {
         return (
             <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-                <h2>Rank: {this.props.rank}</h2>
+                <h2>Votes: {this.props.votes}</h2>
                 <div>
                     <img src={this.props.pic} alt='Superstar'/>
                     <div>
@@ -12,8 +14,13 @@ class Card extends Component {
                         <p>{this.props.email}</p>
                     </div>
                 </div>
+                <ActionButton btnText="Vote Now!" clickhandler={this.onVoteButtonClick}/>
             </div>
         );
+    }
+    onVoteButtonClick = (event) => {
+        console.log('It was clicked : '+this.props.id);
+        
     }
 }
 
