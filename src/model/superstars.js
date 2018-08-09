17003443c00e221beb11fb76a6fcfeebf03ab21c
Sohel Shaikh <rebht78@gmail.com>
2018-08-09 22:50:46 +0530

    Added vote property for superstars

--- src/model/superstars.js
+++ src/model/superstars.js
@@ -1,37 +1,44 @@
 export const superstars = [
     {
+        id:1,
         name:"The Rock",
         email:"rock@robostar.com",
-        rank:0
+        votes:111
     },
     {
+        id:2,
         name:"The Undertaker",
         email:"theundertaker@robostar.com",
-        rank:1
+        votes:1111
     },
     {
+        id:3,
         name:"Roman Reigns",
         email:"roman@robostar.com",
-        rank:2
+        votes:80
     },
     {
+        id:4,
         name:"Seth Rollins",
         email:"seth@robostar.com",
-        rank:3
+        votes:99
     },
     {
+        id:5,
         name:"Dean Ambrose",
         email:"deank@robostar.com",
-        rank:4
+        votes:90
     },
     {
+        id:6,
         name:"A J Styles",
         email:"phenomenalone@robostar.com",
-        rank:5
+        votes:100
     },
     {
+        id:7,
         name:"The Miz",
         email:"miz@robostar.com",
-        rank:100
+        votes:0
     }
 ];
