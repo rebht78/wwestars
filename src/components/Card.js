8c307591b2b43bf4672bb043ca503c992b26e7f8
Sohel Shaikh <rebht78@gmail.com>
2018-08-12 13:42:47 +0530

    Added Delete Button to Card Component, for deleting cards (No function)

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,26 @@
 import React, {Component} from 'react';
 import ActionButton from './ActionButton';
+import './Card.css';
 
 class Card extends Component {
 
     render() {
         return (
-            <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-                <div>
-                    <img src={this.props.pic} alt='Superstar'/>
+            <div className="tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5">
+                    <div className="deletediv">
+                        <ActionButton btnText="X"/>
+                    </div>    
                     <div>
-                        <h2>{this.props.name}</h2>
-                        <p>{this.props.email}</p>
+                        <img src={this.props.pic} alt='Superstar'/>
+                        <div>
+                            <h2>{this.props.name}</h2>
+                            <p>{this.props.email}</p>
+                        </div>
                     </div>
-                </div>
-                <ActionButton btnText="Vote Now!" clickhandler={this.increaseVote}/>
-                <h2>Votes: {this.props.votes}</h2>
+                    <ActionButton btnText="Vote Now!" clickhandler={this.increaseVote}/>
+                    <h2>Votes: {this.props.votes}</h2>
             </div>
+        
         );
     }
     
