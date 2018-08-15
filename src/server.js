2cd5ef1aaa1347c0103b006c7de9cab4e357c0d5
Sohel Shaikh <rebht78@gmail.com>
2018-08-15 13:01:06 +0530

    Added CORS package to server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const fs = require('fs');
+const cors = require('cors');
 const DATA_FILE = "./model/data.json";
 
 const app = express();
+app.use(cors());
 
 app.get("/api/superstars", function(req, res) {
     fs.readFile(DATA_FILE, function(error,data) {
