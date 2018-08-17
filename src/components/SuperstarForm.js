3f1a64f0828cd80d1732bde0d925897239f84175
Sohel Shaikh <rebht78@gmail.com>
2018-08-17 22:29:21 +0530

    Added states to maintain value of textboxes.

--- src/components/SuperstarForm.js
+++ src/components/SuperstarForm.js
@@ -4,6 +4,24 @@ import './SuperstarForm.css';
 
 class SuperstarForm extends Component
 {
+    constructor() {
+        super();
+        this.state = {
+            name:'',
+            email:''
+        }
+    }
+    componentDidMount()
+    {
+        this.setState({name: this.props.name});
+        this.setState({email: this.props.email});
+    }
+    nameChange = (event) => {
+        this.setState({name: event.target.value});
+    };
+    emailChange = (event) => {
+        this.setState({email: event.target.value});
+    };
     render() {
         return (
             <div>
@@ -12,15 +30,15 @@ class SuperstarForm extends Component
                         <h2 className="f10 fw6 ph0 mh0">Update SuperStars</h2>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f8">Name</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" defaultValue={this.props.name}/>
+                            <input onChange={this.nameChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" defaultValue={this.props.name}/>
                         </div>
                         <div className="mv3">
                         <label className="db fw6 lh-copy f8">Email</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" defaultValue={this.props.email}/>
+                            <input onChange={this.emailChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" defaultValue={this.props.email}/>
                         </div>
                         
                             <div className="updatebtn">
-                                <ActionButton btnText="Update"/>
+                                <ActionButton btnText="Update" clickhandler={this.updateButtonClicked}/>
                             </div>
                             <div>
                                 <ActionButton btnText="Cancel"/>
@@ -31,6 +49,9 @@ class SuperstarForm extends Component
                 </div>
             </div>
         );
+    }
+    updateButtonClicked = () => {
+        alert("Name: "+this.state.name);
     }
 }
 
