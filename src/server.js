00e1acd2d853b68e87ef3e112345c20f1425ad2e
Sohel Shaikh <rebht78@gmail.com>
2018-08-19 14:38:57 +0530

    Server now accepts POST request for superstar operation

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const DATA_FILE = "./model/data.json";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.get("/api/superstars", function(req, res) {
     fs.readFile(DATA_FILE, function(error,data) {
@@ -13,5 +14,9 @@ app.get("/api/superstars", function(req, res) {
     });
 });
 
-
+app.post("/api/superstars", function(req,res){
+    console.log('Name: '+req.body.name);
+    console.log('Email: '+req.body.email);
+    res.send('Hey I got it!');
+});
 app.listen(3020, () => console.log("I am started!"));
