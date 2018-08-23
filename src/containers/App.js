93eb86fe7d8a1d189fc4eb63a424ef5c1122916e
Sohel Shaikh <rebht78@gmail.com>
2018-08-23 22:20:08 +0530

    Added Menu component for routing

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,22 @@
 import React, {Component} from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import SuperstarDashBoard from './SuperstarDashBoard';
+import SuperstarForm from '../components/SuperstarForm';
+import Menu from '../components/Menu';
 
-const deadC = () => (
-    <p>This is it!</p>
-);
 class App extends Component
 {
     render() {
         return (
             <BrowserRouter>
                 <div>
-                    <Route path="/" component={SuperstarDashBoard} exact={true}/>
-                    <Route path="/create" component={deadC}/>
+                    <Menu/>
+                    <Switch>
+                        <Route path="/" component={SuperstarDashBoard} exact={true}/>
+                        <Route path="/create" component={SuperstarForm}/>
+                    </Switch>
                 </div>
+                
             </BrowserRouter>
         );
     }
