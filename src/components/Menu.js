9c4f1c1b03b2bb10ca66371232df2a3e55ea8669
Sohel Shaikh <rebht78@gmail.com>
2018-08-23 22:24:28 +0530

    Added Menu component for routing using NavLink (react-router)

---
+++ src/components/Menu.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {NavLink} from 'react-router-dom';
+
+
+
+class Menu extends Component
+{
+    render() {
+        return (
+            <header className="tc">
+                <h1 className="heading">WWE Superstar</h1>
+                <nav class="bt bb mw7 center mt4 ma4">
+                    <NavLink className="f6 f5-l link bg-animate black-80 dib pa3 ph4-l" to="/" exact={true}>Dashboard</NavLink>
+                    <NavLink className="f6 f5-l link bg-animate black-80 dib pa3 ph4-l" to="/create">Create Superstar</NavLink>
+                    <NavLink className="f6 f5-l link bg-animate black-80 dib pa3 ph4-l" to="/Edit">Edit Superstar</NavLink>
+                    <NavLink className="f6 f5-l link bg-animate black-80 dib pa3 ph4-l" to="/help">Help!</NavLink> 
+                </nav>
+               
+            </header>
+        );
+    }
+}
+
+export default Menu;
