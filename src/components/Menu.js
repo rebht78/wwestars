1df604e0b2d2c7795c42c7eec5b63eb09d4b33b7
Sohel Shaikh <rebht78@gmail.com>
2018-08-24 22:18:57 +0530

    Menu now appear bolder and get highlighted when hover over.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,10 +10,10 @@ class Menu extends Component
             <header className="tc">
                 <h1 className="heading">WWE Superstar</h1>
                 <nav class="bt bb mw7 center mt4 ma4">
-                    <NavLink className="f6 f5-l link bg-animate black-80 dib pa3 ph4-l" to="/" exact={true}>Dashboard</NavLink>
-                    <NavLink className="f6 f5-l link bg-animate black-80 dib pa3 ph4-l" to="/create">Create Superstar</NavLink>
-                    <NavLink className="f6 f5-l link bg-animate black-80 dib pa3 ph4-l" to="/Edit">Edit Superstar</NavLink>
-                    <NavLink className="f6 f5-l link bg-animate black-80 dib pa3 ph4-l" to="/help">Help!</NavLink> 
+                    <NavLink className="f6 fw8 f5-l link bg-animate black-80 hover-bg-lightest-blue  dib pa3 ph4-l" to="/" exact={true}>Dashboard</NavLink>
+                    <NavLink className="f6 fw8 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" to="/create">Create Superstar</NavLink>
+                    <NavLink className="f6 fw8 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" to="/Edit">Edit Superstar</NavLink>
+                    <NavLink className="f6 fw8 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" to="/help">Help!</NavLink> 
                 </nav>
                
             </header>
