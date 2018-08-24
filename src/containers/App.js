60022371cba34577db8575d312ec8b25afcb6c32
Sohel Shaikh <rebht78@gmail.com>
2018-08-24 22:19:13 +0530

    added AppRouter component for rendering.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,11 @@
 import React, {Component} from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import SuperstarDashBoard from './SuperstarDashBoard';
-import SuperstarForm from '../components/SuperstarForm';
-import Menu from '../components/Menu';
+import AppRouter from '../routers/AppRouter';
 
 class App extends Component
 {
     render() {
         return (
-            <BrowserRouter>
-                <div>
-                    <Menu/>
-                    <Switch>
-                        <Route path="/" component={SuperstarDashBoard} exact={true}/>
-                        <Route path="/create" component={SuperstarForm}/>
-                    </Switch>
-                </div>
-                
-            </BrowserRouter>
+            <AppRouter/>
         );
     }
 }
